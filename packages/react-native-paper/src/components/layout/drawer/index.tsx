443ceb36acef7c3161f766fc420533db79b79536
abdellah711 <alaouiabdellah711@gmail.com>
2023-07-09 23:42:05 +0100

    feat(react-native-paper): implement drawer content component

---
+++ packages/react-native-paper/src/components/layout/drawer/index.tsx
@@ -0,0 +1,173 @@
+import React, { FC } from 'react'
+import {
+    useGo,
+    useMenu,
+    ITreeMenu,
+    CanAccess,
+    useRefineContext,
+    useTranslate,
+    useWarnAboutChange,
+    useLogout,
+    useIsExistAuthentication
+} from '@refinedev/core'
+import {
+    Alert,
+    Platform
+} from 'react-native'
+
+import { RefineLayoutDrawerContentProps } from '../types'
+import { Button, Divider, Drawer, Surface } from 'react-native-paper'
+
+export const DrawerContent: FC<RefineLayoutDrawerContentProps> = ({
+    Title,
+    render,
+    meta
+}) => {
+    const { menuItems, selectedKey } = useMenu({ meta })
+    const go = useGo()
+    const { hasDashboard } = useRefineContext();
+    const translate = useTranslate()
+    const { warnWhen, setWarnWhen } = useWarnAboutChange();
+    const { mutate: mutateLogout } = useLogout()
+    const isExistAuthentication = useIsExistAuthentication();
+
+    const TitleToRender = Title 
+
+    const renderTreeView = (tree: ITreeMenu[]) => {
+        return tree.map((item) => {
+            const { label: legacyLabel, name, icon: legacyIcon, children, meta, list } = item;
+            const icon = legacyIcon ?? meta?.icon;
+            const label = legacyLabel ?? meta?.label;
+            const isSelected = item.key === selectedKey;
+            const isParent = children.length > 0;
+
+            if (isParent) return (
+                <CanAccess
+                    key={item.key}
+                    resource={name.toLowerCase()}
+                    action="list"
+                    params={{
+                        resource: item,
+                    }}
+                >
+                    <Drawer.CollapsedItem
+                        label={label}
+                        active={isSelected}
+                        focusedIcon={typeof icon === 'string' ? icon : () => icon}
+                        unfocusedIcon={typeof icon === 'string' ? icon : () => icon}
+                    >
+                        {renderTreeView(children)}
+                    </Drawer.CollapsedItem>
+                </CanAccess>
+            )
+
+            return (
+                <CanAccess
+                    key={item.key}
+                    resource={name.toLowerCase()}
+                    action="list"
+                    params={{
+                        resource: item,
+                    }}
+                >
+                    <Drawer.Item
+                        label={label ?? ''}
+                        icon={typeof icon === 'string' ? icon : () => icon}
+                        onPress={() => go({ to: list as string, type: 'push' })}
+                        active={isSelected}
+                    />
+                </CanAccess>
+            );
+        });
+    };
+
+    const items = renderTreeView(menuItems);
+
+    const dashboard = hasDashboard ? (
+        <CanAccess resource="dashboard" action="list">
+            <Drawer.Item
+                label={translate("dashboard.title", "Dashboard")}
+                icon="view-dashboard"
+                onPress={() => go({ to: '/', type: 'push' })}
+                active={selectedKey === '/'}
+            />
+        </CanAccess>
+    ) : null;
+
+    const handleLogout = () => {
+        if (warnWhen) {
+            if (Platform.OS === 'web') {
+                const confirm = window.confirm(
+                    translate(
+                        "warnWhenUnsavedChanges",
+                        "Are you sure you want to leave? You have unsaved changes.",
+                    ),
+                );
+
+                if (confirm) {
+                    setWarnWhen(false);
+                    mutateLogout();
+                }
+            } else {
+                Alert.alert(
+                    translate(
+                        "warnWhenUnsavedChanges",
+                        "Are you sure you want to leave? You have unsaved changes.",
+                    ),
+                    undefined,
+                    [
+                        {
+                            text: translate("buttons.cancel", "Cancel"),
+                        },
+                        {
+                            text: translate("buttons.ok", "OK"),
+                            onPress: () => {
+                                setWarnWhen(false);
+                                mutateLogout();
+                            }
+                        }
+                    ]
+                )
+            }
+        } else {
+            mutateLogout();
+        }
+    };
+
+    const logout = isExistAuthentication && (
+        <>
+            <Divider style={{marginTop:'auto', marginBottom: 10}}/>
+            <Button
+                key="logout"
+                onPress={handleLogout}
+                icon="logout"
+            >
+                {translate("buttons.logout", "Logout")}
+            </Button>
+        </>
+    );
+
+    const renderSider = () => {
+        if (render) {
+            return render({
+                dashboard,
+                items,
+                logout,
+            });
+        }
+        return (
+            <>
+                {dashboard}
+                {items}
+                {logout}
+            </>
+        );
+    };
+
+    return (
+        <Surface style={{ flex: 1, padding: 10 }}>
+            {TitleToRender && <TitleToRender />}
+            {renderSider()}
+        </Surface>
+    )
+}
