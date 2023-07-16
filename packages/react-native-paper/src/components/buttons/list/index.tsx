132be74799b7a028efaf113bd27f854660701ec2
abdellah711 <alaouiabdellah711@gmail.com>
2023-07-16 13:46:05 +0100

    feat(react-native-paper): implement `ListButton`

---
+++ packages/react-native-paper/src/components/buttons/list/index.tsx
@@ -0,0 +1,93 @@
+import React, { useContext } from "react";
+import { ListButtonProps } from "../types";
+import { AccessControlContext, useCan, useGo, useNavigation, useResource, useTranslate, useUserFriendlyName, userFriendlyResourceName } from "@refinedev/core";
+import { Button, ButtonProps, FAB, FABProps } from "react-native-paper";
+
+
+export const ListButton: React.FC<ListButtonProps> = ({
+    resource: resourceNameFromProps,
+    hideText = false,
+    accessControl,
+    meta,
+    children,
+    onPress,
+    asFAB = false,
+    ...rest
+}) => {
+    const accessControlContext = useContext(AccessControlContext);
+
+    const accessControlEnabled =
+        accessControl?.enabled ??
+        accessControlContext.options.buttons.enableAccessControl;
+
+    const hideIfUnauthorized =
+        accessControl?.hideIfUnauthorized ??
+        accessControlContext.options.buttons.hideIfUnauthorized;
+
+    const translate = useTranslate();
+
+    const getUserFriendlyName = useUserFriendlyName?.() ?? userFriendlyResourceName;
+
+
+    const { listUrl: generateListUrl } = useNavigation();
+
+    const { resource, identifier } = useResource(resourceNameFromProps);
+
+
+    const { data } = useCan({
+        resource: resource?.name,
+        action: "list",
+        queryOptions: {
+            enabled: accessControlEnabled,
+        },
+        params: {
+            resource,
+        },
+    });
+
+    const go = useGo()
+
+    const listUrl = resource ? generateListUrl(resource, meta) : "";
+
+
+    if (accessControlEnabled && hideIfUnauthorized && !data?.can) {
+        return null;
+    }
+
+    const text = !hideText && (children ??
+        translate(
+            `${identifier ??
+            resourceNameFromProps
+            }.titles.list`,
+            getUserFriendlyName(
+                resource?.meta?.label ??
+                resource?.label ??
+                identifier ??
+                resource?.name ??
+                resourceNameFromProps,
+                "plural",
+            ),
+        ));
+
+    console.log({ text, identifier })
+
+    return asFAB ? (
+        <FAB
+            label={typeof text === 'string' ? text : undefined}
+            icon="format-list-bulleted"
+            mode="elevated"
+            disabled={data?.can === false}
+            style={{ position: 'absolute', margin: 16, right: 0, bottom: 0 }}
+            onPress={() => go({ to: listUrl, type: 'push' })}
+            {...rest as FABProps}
+        />
+    ) : (
+        <Button
+            disabled={data?.can === false}
+            icon="format-list-bulleted"
+            mode="contained"
+            onPress={() => go({ to: listUrl, type: 'push' })}
+            {...rest as ButtonProps}
+        >{text}</Button>
+    )
+}
--- packages/react-native-paper/src/components/buttons/types.ts
+++ packages/react-native-paper/src/components/buttons/types.ts
@@ -4,14 +4,18 @@ import {
     RefineShowButtonProps,
     RefineEditButtonProps,
     RefineDeleteButtonProps,
+    RefineRefreshButtonProps,
+    RefineListButtonProps,
 } from '@refinedev/ui-types'
 import { ButtonProps, FABProps } from 'react-native-paper'
 
+// a button by default
 export type ButtonOrFAB = (
     | Partial<ButtonProps> & { asFAB?: false }
     | Partial<FABProps> & { asFAB: true }
 )
 
+// a FAB by default
 export type FABOrButton = (
     | Partial<ButtonProps> & { asFAB: false }
     | Partial<FABProps> & { asFAB?: true }
@@ -26,3 +30,7 @@ export type ShowButtonProps = RefineShowButtonProps<ButtonOrFAB>
 export type EditButtonProps = RefineEditButtonProps<ButtonOrFAB>
 
 export type DeleteButtonProps = RefineDeleteButtonProps<ButtonOrFAB>
+
+export type RefreshButtonProps = RefineRefreshButtonProps<ButtonOrFAB>
+
+export type ListButtonProps = RefineListButtonProps<ButtonOrFAB>
